cff93b48d0ee750c035cb868be118d52ae18ef0f
1dle0ne <147718885+salievyt@users.noreply.github.com>
2026-01-08 16:38:15 +0600

    home work 3

---
+++ mouth-1/lesson-3/hm3.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  List<int> nums = [1,32,7,77,34,45,32,53,75,44,2,37,22];
+  for (var i in nums) {
+    if (i > 0 && i < 10){
+      continue;
+    }
+    print(i);
+  }
+
+  print('---------------');
+
+
+  List<String> names = ["Alihan","Beka","Nur","Zeka","Abu","Malik","Jumabek","Aziz","Kamal","Abdullah", "Faha"];
+  for (var name in names) {
+    if (name.startsWith('A') || name.startsWith('a')) {
+      print(name);
+    }
+    if (name.length > 10) {
+      print(name);
+      break;
+    }
+  }
+
+  print('---------------');
+
+  List<String> index = [];
+  while (index.length < 10) {
+    index.add("${index.length + 1}");
+    print(index);
+  }
+
+}
