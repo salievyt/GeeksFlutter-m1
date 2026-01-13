f54a393b994b8caf19940cf7cfbe7ec631c06303
1dle0ne <147718885+salievyt@users.noreply.github.com>
2026-01-13 12:18:34 +0600

    hm4 fix

--- mouth-1/lesson-4/hm4.dart
+++ mouth-1/lesson-4/hm4.dart
@@ -1,3 +1,14 @@
 void main(List<String> args) {
-  
+  List<int> nums = [4,34,52,3,25,37,65,22];
+  print(sum_positive_even(nums));
+}
+
+int sum_positive_even(List<int> numbers) {
+  int sum = 0;
+  for (int number in numbers) {
+    if (number > 0 && number % 2 == 0) {
+      sum += number;
+    }
+  }
+  return sum;
 }
